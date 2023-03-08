57f2cd9aa29ebd962100cb3f28261da5569871de
Giorgos <giorgoskltzds@gmail.com>
2023-03-08 22:13:08 +0100

    add models and admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Receipt
+
+@admin.register(Receipt)
+class ReceiptAdmin(admin.ModelAdmin):
+    list_display = ("issue_date", "price_per_liter", "total_value", "calculate_tax_value", "calculate_net_value", "calculate_liters")
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+TAX = 0.24
+
+class Receipt(models.Model):
+    issue_date = models.DateTimeField()
+    price_per_liter = models.FloatField()
+    total_value = models.FloatField()
+    
+    def __str__(self):
+        return "Paid " + str(self.total_value) + " " + "At: " + str(self.price_per_liter) + " per liter"
+
+    @property
+    def calculate_tax_value(self):
+        tax_value = self.total_value * TAX
+        return tax_value
+
+    @property
+    def calculate_net_value(self):
+        net_value = self.total_value - self.calculate_tax_value
+        return net_value
+    
+    @property
+    def calculate_liters(self):
+        liters = self.total_value / self.price_per_liter
+        return liters
+    
+    class Meta:
+        ordering = ['issue_date']
+    
